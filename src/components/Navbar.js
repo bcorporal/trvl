b490b63e4ce3dec472653ac44eb2c3f300ccfdae
bcorporal <bcorporal@gmail.com>
2022-06-08 12:19:37 -0400

    debugging syntax issues with newer version of react

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
       <nav className='navbar'>
       <div className='navbar-container'>
       <Link to="/" className='navbar-logo' onClick= {closeMobileMenu}>
-      TRVL <i class="fa-brands fa-typo3"></i> 
+      TRVL <i className="fa-brands fa-typo3"></i> 
       </Link>
       <div className='menu-icon' onClick={handleClick}>
       <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
