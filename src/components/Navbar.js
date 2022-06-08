9545ab93080b071220cc112d052fe0c0edf5cd51
bcorporal <bcorporal@gmail.com>
2022-06-08 14:18:05 -0400

    logo change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
       <nav className='navbar'>
       <div className='navbar-container'>
       <Link to="/" className='navbar-logo' onClick= {closeMobileMenu}>
-      TRVL <i className="fa-brands fa-typo3"></i> 
+      XPLORE <i className="fa-solid fa-passport"></i> 
       </Link>
       <div className='menu-icon' onClick={handleClick}>
       <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
