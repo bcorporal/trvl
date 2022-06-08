a009a5bb0fffd1f4995c6a0a74913abbe6d605f7
bcorporal <bcorporal@gmail.com>
2022-06-08 10:53:31 -0400

    navbar button setup for signup, working on css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,21 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+      if(window.innerWidth <= 960) {
+        setButton(false);
+      } else {
+        setButton(true);
+      }
+      };
+
+      window.addEventListener('resize', showButton);
+    
 
   return (
       <>
@@ -23,7 +36,23 @@ function Navbar() {
       Home
       </Link>
       </li>
+      <li className='nav-item'>
+      <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+      Services
+      </Link>
+      </li>
+      <li className='nav-item'>
+      <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+      Products
+      </Link>
+      </li>
+      <li className='nav-item'>
+      <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+      Sign up
+      </Link>
+      </li>
       </ul>
+      {button && <button buttonStyle='btn--outline'>SIGN UP</button>}
       </div>
       </nav>
       </>
