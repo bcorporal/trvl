fd68139cf897d7fbe1249cbb5635c64e4d825302
bcorporal <bcorporal@gmail.com>
2022-06-08 14:01:36 -0400

    card section complete, mobile responsive

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,7 +11,29 @@ function Cards() {
     <ul className='cards__items'>
     <CardItem
     src='images/img-9.jpg'
-    text='explore'
+    text='explore...'
+    label='Adventure'
+    path='/services'/>
+    <CardItem
+    src='images/img-2.jpg'
+    text='explore...'
+    label='Adventure'
+    path='/services'/>  
+    </ul>
+    <ul className='cards__items'>
+    <CardItem
+    src='images/img-3.jpg'
+    text='explore...'
+    label='Adventure'
+    path='/services'/>
+    <CardItem
+    src='images/img-4.jpg'
+    text='explore...'
+    label='Adventure'
+    path='/services'/>
+    <CardItem
+    src='images/img-8.jpg'
+    text='explore...'
     label='Adventure'
     path='/services'/>
     </ul>
